764a1839b5a83a4a5c061b9e85834bda2e94202a
nameless223 <d665027@gmail.com>
2024-11-14 12:40:13 -0600

    comitttt

--- EmployeeData.cpp
+++ EmployeeData.cpp
@@ -1,45 +1,77 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+
 
 using namespace std;
 
 class EmployData
 {
 public:
-	struct employ
+	struct allemploy
 	{
 		string name;
-		int age;
-		string dob;	
+		int age=;
+		string dob=;
 	};
 
-	employ employees[6] =
+	vector <employ> dataset;
+
+	void feeddata() 
 	{
-		{"Adam\t", 23, "\tFebruary 21"},
-		{"Joe\t", 22, "\tJuly 10"},
-		{"Tom\t", 34, "\tJune 9"},
-		{"John\t", 44, "\tAugust 2"},
-		{"Lee\t", 19, "\tMay 13"},
-		{"Sally\t", 40, "\tSeptember 6"},
-	};
+		dataset =
+		{
+			{"Adam", 23, "02/21"},
+			{"Joe", 22, "06/10"},
+			{"Tom", 34, "05/09"},
+			{"John", 44, "07/02"},
+			{"Lee", 19, "09/13"},
+			{"Sally", 40, "09/06"},
+		};
 
+		
+	};
+	
+	static bool gtlComp(employ a, employ b)
+	{
+		return a.age < b.age;
+	}
+	static bool ltgComp(employ a, employ b)
+	{
+		return a.age > b.age;
+	}
+
+	void gtlSort()
+	{
+		sort(dataset.begin(), dataset.end(), gtlComp);
+	}
+	void ltgSort()
+	{
+		sort(dataset.begin(), dataset.end(), ltgComp);
+	}
+	void Print()
+	{
+		for (int i = 0; i < dataset.size(); i++)
+		{
+			cout << dataset[i].name << " " << dataset[i].age << " " << dataset[i].dob << endl;
+		}
+	}
+	void gtL(employ a, employ b)
+	{
+		gtlComp(a, b);
+		gtlSort();
+		Print();
+	}
+private:
+	int choice;
 };
 
 
 
 int main()
-{
+{	
 	EmployData f1;
-	int choice;
-	cout << "Would you like to sort the data?" << endl;
-	cin >> choice;
 	
-	
-	sort(begin(f1.employees), end(f1.employees));
-	
-	cout << f1.employees;
-		
-	
-	
+	f1.gtL();
 }
